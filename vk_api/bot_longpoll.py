f4452f7ef29202b3fe397d7656c0be19b47f3d9f
Vasi4n <51722856+Vasi4n@users.noreply.github.com>
2019-06-12 02:14:06 +0500

    Update bot_longpoll.py

--- vk_api/bot_longpoll.py
+++ vk_api/bot_longpoll.py
@@ -273,3 +273,19 @@ class VkBotLongPoll(object):
         while True:
             for event in self.check():
                 yield event
+                class VkBot:
+
+    def __init__(self, user_id):
+    
+        print("Создан объект бота!")
+        self._USER_ID = user_id
+        self._USERNAME = self._get_user_name_from_vk_id(user_id)
+        
+        self._COMMANDS = ["ПРИВЕТ", "ПОГОДА", "ВРЕМЯ", "ПОКА"]
+        def _get_user_name_from_vk_id(self, user_id):
+    request = requests.get("https://vk.com/id"+str(user_id))
+    bs = bs4.BeautifulSoup(request.text, "html.parser")
+    
+    user_name = self._clean_all_tag_from_str(bs.findAll("title")[0])
+    
+    return user_name.split()[0]
